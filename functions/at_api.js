7bb848af57136cc5adb8545b2f6975bf2975a77b
Scott Bram <scottbram@users.noreply.github.com>
2019-07-05 17:17:06 -0600

    Moved Google Fonts to top of <link>s to see if it does anything for FOUT; added informative code comments and project details to README

--- functions/at_api.js
+++ functions/at_api.js
@@ -1,4 +1,5 @@
 const Airtable = require('airtable')
+// The next 2 lines refer to environment variables configured in Netlify settings (found in "Site settings > Build & deploy > Environment" as of this writing)
 const { AIRTABLE_API_KEY } = process.env
 const { AIRTABLE_BASE_ID } = process.env
 const at_obj = new Airtable({
